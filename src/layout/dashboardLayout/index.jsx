dc4235e0cc615bae8d8d1b9a2fd8235f71dfdb84
Ali Kooshesh <alikooshesh007@gmail.com>
2025-08-01 03:00:01 +0330

    major changes

--- src/layout/dashboardLayout/index.jsx
+++ src/layout/dashboardLayout/index.jsx
@@ -1,6 +1,13 @@
 "use client";
 
-import { Back, DocumentFilter, FolderAdd, Home2, Profile2User, Ship } from "iconsax-reactjs";
+import {
+  Back,
+  DocumentFilter,
+  FolderAdd,
+  Home2,
+  Profile2User,
+  Ship,
+} from "iconsax-reactjs";
 import HeaderNav from "./headerNav";
 import Link from "next/link";
 import { useEffect, useState } from "react";
@@ -67,14 +74,16 @@ const DashboardLayout = ({ children }) => {
     <div className="w-screen min-h-screen bg-[#F5F6FA]">
       <div className="hidden xl:block w-full h-[80px] px-[24px] py-[8px] bg-white">
         <div className="w-full flex items-center justify-between">
-          <img className="h-[64px]" src="/images/dashboard/navbar/logo.png" />
-          <div className="flex items-center gap-[32px]">
-            <div className="flex items-center gap-[36px]">
-              {navList.map((item) => (
-                <HeaderNav key={item.url} {...item} />
-              ))}
+          <div className="flex items-center gap-[42px]">
+            <img className="h-[64px]" src="/images/dashboard/navbar/logo.png" />
+            <div className="flex items-center gap-[32px]">
+              <div className="flex items-center gap-[36px]">
+                {navList.map((item) => (
+                  <HeaderNav key={item.url} {...item} />
+                ))}
+              </div>
+              <div className="bg-white w-[311px] h-[44px]" />
             </div>
-            <div className="bg-white w-[311px] h-[44px]" />
           </div>
           <div
             className={`size-[64px] flex items-center justify-center rounded-full border-[2px] ${
@@ -89,26 +98,25 @@ const DashboardLayout = ({ children }) => {
             >
               <img
                 className="size-[48px] rounded-full"
-                src={
-                  profileImage ??
-                  "/t.png"
-                }
+                src={profileImage ?? "/t.png"}
               />
             </Link>
           </div>
         </div>
       </div>
-      {pathname === '/dashboard' ? <DashboardMainPageHeader name={name} profileImage={profileImage}/> :
+      {pathname === "/dashboard" ? (
+        <DashboardMainPageHeader name={name} profileImage={profileImage} />
+      ) : (
         <button
-        onClick={() => {
-          window.history.back();
-        }}
-        className="w-full xl:hidden px-[16px] pt-[24px] flex items-center gap-[8px] text-[#6B7BFF]"
-      >
-        <Back size={24} />
-        <p className="font-[500] text-[16px]">Back</p>
-      </button>
-      }
+          onClick={() => {
+            window.history.back();
+          }}
+          className="w-full xl:hidden px-[16px] pt-[24px] flex items-center gap-[8px] text-[#6B7BFF]"
+        >
+          <Back size={24} />
+          <p className="font-[500] text-[16px]">Back</p>
+        </button>
+      )}
       {children}
     </div>
   );
@@ -122,24 +130,17 @@ const DashboardMainPageHeader = ({ name, profileImage }) => {
       <Link href={"/dashboard/profile"} className="size-[48px] rounded-full">
         <img
           className="size-[48px] rounded-full"
-          src={
-            profileImage ??
-            "/t.png"
-          }
+          src={profileImage ?? "/t.png"}
         />
       </Link>
 
       <div className="flex flex-col items-center justify-center gap-[4px]">
-        <p className="font-[500] text-[16px] text-[#7C7C7C]">
-        Welcome aboard 
-        </p>
-        <p className="font-[600] text-[14px] text-[#1E1E1E]">
-          {name}
-        </p>
+        <p className="font-[500] text-[16px] text-[#7C7C7C]">Welcome aboard</p>
+        <p className="font-[600] text-[14px] text-[#1E1E1E]">{name}</p>
       </div>
 
       <button className="size-[48px] rounded-full flex items-center justify-center bg-white border-[1px] border-[#2996E8]">
-        <DocumentFilter size={24} color="#2996E8"/>
+        <DocumentFilter size={24} color="#2996E8" />
       </button>
     </div>
   );
