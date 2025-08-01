d83a886bae48ff3e32215b6a602703a87877ca4a
Ali Kooshesh <alikooshesh007@gmail.com>
2025-08-01 23:47:31 +0330

    add statics

--- src/data/staticLists.js
+++ src/data/staticLists.js
@@ -1,6 +1,9 @@
-export const vesselTypeList = ['vessel type 1' , 'vessel type 2']
-export const productCategoryList = ['category 1' , 'category 2']
+export const vesselTypeList = ['Container Ship' , 'Bulk Carrier' , 'Tanker', 'LNG Carrier', 'LPG Carrier']
+export const productCategoryList = ['Bulk Cargo' , 'Containerized Cargo', 'Reefer Cargo' , 'Liquid Cargo', 'Dry Cargo']
 export const productUnitByCategortList = {
-    "category 1" : ['unit 1 / category 1' , 'unit 2 / category 1'],
-    "category 2" : ['unit 1 / category 2' , 'unit 2 / category 2']
+    "Bulk Cargo" : ['Metric Ton (MT)' , 'Cubic Meter (CBM)', 'Cubic Feet (ft³)', 'Bag (Bags)','Pallet (Plt)'],
+    "Containerized Cargo" : ['TEU' , 'FEU', '40 FtHQ', 'Kilogram (kg)', 'Metric Ton (MT)', 'Cubic Meter (CBM)', 'Box (Bx)', 'Carton (Ctn)','Pallet (Plt)'],
+    "Reefer Cargo" : ['Kilogram (kg)','Metric Ton (MT)','Box (Bx)','Carton (Ctn)','Pallet (Plt)','Cubic Meter (CBM)'],
+    "Liquid Cargo" : ['Kilogram (kg)','Metric Ton (MT)','Cubic Meter (CBM)','Liter (L)','Cubic Feet (ft³)','Barrel (bbl)', 'Gallon (gal)','Cubic Meter (m³)'],
+    "Dry Cargo" : ['Kilogram (kg)','Metric Ton (MT)','Pound (lb)','Unit (pcs)','Box (Bx)','Carton (Ctn)','Bag (Bags)','Pallet (Plt)','Square Meter (m²)', 'Square Foot (ft²)']
 }
